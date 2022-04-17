066f23e2a1829b5d1ab36474e47fd4e6210b4bbe
romil-rc <romil.chimankar@gmail.com>
2022-04-17 17:13:49 +0530

    render check

--- src/components/Entertainment.js
+++ src/components/Entertainment.js
@@ -9,15 +9,36 @@ export default function Bollywood({ title, country, category, pageSize }) {
     const [articles, setArticles] = useState([]);
     const [visible, setVisible] = useState(pageSize);
     const [loading, setLoading] = useState(false);
+
     useEffect(() => {
-        const fetchData = async() => {
-            setLoading(true);
-            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=9d59c1a70aa84558a50aa031bdeb94e9`);
-            setArticles(response.data.articles);
+        // const fetchData = async() => {
+        //     setLoading(true);
+        //     const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=9d59c1a70aa84558a50aa031bdeb94e9`);
+        //     setArticles(response.data.articles);
+        //     setLoading(false);
+        // }
+        // fetchData();
+
+        var options = {
+            method: 'GET',
+            url: 'https://api.newscatcherapi.com/v2/search',
+            params: {q: 'Bollywood', lang: 'en', countries: "IN", sort_by: 'relevancy', page: '1'},
+            headers: {
+              'x-api-key': 'ftdhXNVdbICzaKmkkAm5rhMrHn-Fat2fOv4WuUzzsy0'
+            }
+          };
+          
+          setLoading(true);
+          axios.request(options).then(function (response) {
+                setArticles(response.data.articles);
+                // console.log(response.data.articles);
+            }).catch(function (error) {
+                console.error(error);
+            });
             setLoading(false);
-        }
-        fetchData();
-    }, [country, category])
+
+
+    }, [])
     // console.log(articles);
     const loadMore = () => {
         setVisible(visible + 5);
@@ -46,15 +67,15 @@ export default function Bollywood({ title, country, category, pageSize }) {
                             <div className='container'>
                                 <div className='row'>
                                     <div className='col-md-6'>
-                                        { article.urlToImage ? <img src={article.urlToImage} style={{height: "232px", width: "316px", borderRadius: "0px"}} alt="" /> : <img src="https://askleo.askleomedia.com/wp-content/uploads/2004/06/no_image-300x245.jpg" style={{height: "232px", width: "316px", borderRadius: "0px"}} alt="" />}
+                                        { article.media ? <img src={article.media} style={{height: "232px", width: "316px", borderRadius: "0px"}} alt="" /> : <img src="https://askleo.askleomedia.com/wp-content/uploads/2004/06/no_image-300x245.jpg" style={{height: "232px", width: "316px", borderRadius: "0px"}} alt="" />}
                                     </div>
                                     <div className='col-md-6'>
                                         <div>
-                                            <h5 className='text-truncate fw-bold'><a href={article.url} className="mtlb">{article.title}</a></h5>
-                                            { article.description ? <p>{article.description}</p> : <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident obcaecati quis vero veritatis unde consequatur, tenetur excepturi soluta totam maxime quasi necessitatibus quo sunt eligendi id eaque repellat fugiat nulla!</p>}
+                                            <h5 className='text-truncate fw-bold'><a href={article.link} className="mtlb">{article.title}</a></h5>
+                                            { article.excerpt ? <p>{article.excerpt}</p> : <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident obcaecati quis vero veritatis unde consequatur, tenetur excepturi soluta totam maxime quasi necessitatibus quo sunt eligendi id eaque repellat fugiat nulla!</p>}
                                         </div>
                                         <div className=''>
-                                            <p className='text-capitalize align-bottom'><span className='fw-bold'>{category}</span> / {new Date(article.publishedAt).toLocaleString('default', { month: 'long' })} {new Date(article.publishedAt).getDate()} {new Date(article.publishedAt).getFullYear()} </p>
+                                            <p className='text-capitalize align-bottom'><span className='fw-bold'>{category}</span> / {new Date(article.published_date).toLocaleString('default', { month: 'long' })} {new Date(article.published_date).getDate()} {new Date(article.published_date).getFullYear()} </p>
                                         </div>
                                     </div>
                                 </div>
@@ -79,11 +100,11 @@ export default function Bollywood({ title, country, category, pageSize }) {
                     {articles.slice(-1).map((element, i) => {
                             return (
                                 <>
-                                    <img src={element.urlToImage} alt="mountains" id='pbi'/>
+                                    <img src={element.media} alt="mountains" id='pbi'/>
                                     <div className="row mt-4 mb-3">
                                         <div className="col-md-7">
-                                            <h5 id='ptpit'><a href={element.url} className='mtlb'>{element.title.slice(0, 30)}</a></h5><br />
-                                            <p className='text-muted text-capitalize'><span className='fw-bold'>{category}</span> / {new Date(element.publishedAt).toLocaleString('default', { month: 'long' })} {new Date(element.publishedAt).getDate()} {new Date(element.publishedAt).getFullYear()} </p>
+                                            <h5 id='ptpit'><a href={element.link} className='mtlb'>{element.title.slice(0, 30)}</a></h5><br />
+                                            <p className='text-muted text-capitalize'><span className='fw-bold'>{category}</span> / {new Date(element.published_date).toLocaleString('default', { month: 'long' })} {new Date(element.published_date).getDate()} {new Date(element.published_date).getFullYear()} </p>
                                         </div>
                                         <div className="col-md-5">
                                             <h2 id='post-number'><b>{i+1}</b></h2>
@@ -98,13 +119,13 @@ export default function Bollywood({ title, country, category, pageSize }) {
                                     <div className="card mb-5 small-right-card post-show-card" style={{width: '100%'}}>
                                         <div className="row g-0">
                                             <div className="col-md-6 tpct">
-                                                <img src={elements.urlToImage} className="img-fluid rounded-start tpkci" style={{borderRadius: "0px"}} alt="castle"/>
+                                                <img src={elements.media} className="img-fluid rounded-start tpkci" style={{borderRadius: "0px"}} alt="castle"/>
                                             </div>
                                             <div className="col-md-5">
                                                 <div className="card-body pcardb">
-                                                    <h5 className="card-title tpkct fs-6"><a href={elements.url} className='mtlb' style={{width: "150px"}}>{elements.title.slice(0, 30)}</a></h5><br /><br />
+                                                    <h5 className="card-title tpkct fs-6"><a href={elements.link} className='mtlb' style={{width: "150px"}}>{elements.title.slice(0, 30)}</a></h5><br /><br />
                                                     {/* <p className="card-text really-small"><small className={elements.cardTextMutedClass}><b>{elements.categoryMuted}</b>{elements.categoryMutedDate}</small></p> */}
-                                                    <p className='card-text really-small text-capitalize'><span className='fw-bold'>{category}</span> / {new Date(elements.publishedAt).toLocaleString('default', { month: 'long' })} {new Date(elements.publishedAt).getDate()} {new Date(elements.publishedAt).getFullYear()} </p>
+                                                    <p className='card-text really-small text-capitalize'><span className='fw-bold'>{category}</span> / {new Date(elements.published_date).toLocaleString('default', { month: 'long' })} {new Date(elements.published_date).getDate()} {new Date(elements.published_date).getFullYear()} </p>
                                                 </div>
                                             </div>
                                             <div className="col-md-1">
